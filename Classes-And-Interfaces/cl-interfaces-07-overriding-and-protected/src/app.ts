eac3d03e5e7c4c745f4dc48d3cd5f8bedec9c52f
StanislavHrusanov <stanislavhrusanov@gmail.com>
2024-04-06 13:46:43 +0300

    Add cl-interfaces-07-overriding-and-protected

---
+++ Classes-And-Interfaces/cl-interfaces-07-overriding-and-protected/src/app.ts
@@ -0,0 +1,80 @@
+class Department {
+  protected employees: string[] = [];
+
+  constructor(private readonly id: string, public name: string) {
+
+  }
+
+  describe(this: Department) {
+    console.log(`Department (${this.id}): ${this.name}`);
+  }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInfo() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
+}
+
+class ITDepartment extends Department {
+  admins: string[];
+  constructor(id: string, admins: string[]) {
+    super(id, 'IT');
+    this.admins = admins;
+  }
+
+}
+
+class AccountingDepartment extends Department {
+  constructor(id: string, private reports: string[]) {
+    super(id, 'Accounting');
+  }
+
+  addEmployee(employee: string) {
+    if (employee === 'Stan') {
+      return;
+    }
+    this.employees.push(employee);
+  }
+
+  addReports(text: string) {
+    this.reports.push(text);
+  }
+
+  printReports() {
+    console.log(this.reports);
+  }
+}
+
+const it = new ITDepartment('d3', ['Stan']);
+
+it.addEmployee('John');
+it.addEmployee('Peter');
+
+it.describe();
+it.name = 'New name';
+it.printEmployeeInfo();
+
+console.log(it);
+
+it.describe();
+
+const accounting = new AccountingDepartment('d4', []);
+
+accounting.describe();
+
+console.log(accounting);
+
+accounting.addReports('Something went wrong...');
+
+accounting.printReports();
+
+accounting.addEmployee('Stan');
+accounting.addEmployee('Steven');
+
+accounting.printEmployeeInfo();
+
+console.log(accounting);
