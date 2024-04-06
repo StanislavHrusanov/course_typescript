63161e7f4846645f3d0df0c68696bc2d71ac5aef
StanislavHrusanov <stanislavhrusanov@gmail.com>
2024-04-06 15:31:49 +0300

    Add cl-interfaces-09-abstract

---
+++ Classes-And-Interfaces/cl-interfaces-09-abstract/src/app.ts
@@ -0,0 +1,114 @@
+abstract class Department {
+  static fiscalYear = 2023;
+  protected employees: string[] = [];
+
+  constructor(protected readonly id: string, public name: string) {
+
+  }
+
+  static createEmployee(name: string) {
+    return { name: name };
+  }
+
+  abstract describe(this: Department): void;
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInfo() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
+}
+
+class ITDepartment extends Department {
+  admins: string[];
+  constructor(id: string, admins: string[]) {
+    super(id, 'IT');
+    this.admins = admins;
+  }
+
+  describe() {
+    console.log(`IT Department with ID - ${this.id}`);
+  }
+}
+
+class AccountingDepartment extends Department {
+  private lastReport: string;
+
+  get mostRecentReport() {
+    if (this.lastReport) {
+      return this.lastReport;
+    }
+    throw new Error('No reports found.');
+  }
+
+  set mostRecentReport(value: string) {
+    if (!value) {
+      throw new Error('Please pass in a valid value!')
+    }
+    this.addReport(value);
+  }
+
+  constructor(id: string, private reports: string[]) {
+    super(id, 'Accounting');
+    this.lastReport = reports[0];
+  }
+
+  describe() {
+    console.log(`Accounting Department with ID - ${this.id}`);
+  }
+
+  addEmployee(employee: string) {
+    if (employee === 'Stan') {
+      return;
+    }
+    this.employees.push(employee);
+  }
+
+  addReport(text: string) {
+    this.reports.push(text);
+    this.lastReport = text;
+  }
+
+  printReports() {
+    console.log(this.reports);
+  }
+}
+
+const employee1 = Department.createEmployee('Ivan');
+console.log(employee1, Department.fiscalYear);
+
+const it = new ITDepartment('d3', ['Stan']);
+
+it.addEmployee('John');
+it.addEmployee('Peter');
+
+it.describe();
+it.name = 'New name';
+it.printEmployeeInfo();
+
+console.log(it);
+
+it.describe();
+
+const accounting = new AccountingDepartment('d4', []);
+
+accounting.describe();
+
+console.log(accounting);
+
+accounting.mostRecentReport = 'Year End Report'
+accounting.addReport('Something went wrong...');
+console.log(accounting.mostRecentReport);
+
+
+accounting.printReports();
+
+accounting.addEmployee('Stan');
+accounting.addEmployee('Steven');
+
+accounting.printEmployeeInfo();
+
+console.log(accounting);
