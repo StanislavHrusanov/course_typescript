4400daa354d6f50bfae2c6e7600a99db31cb8d54
StanislavHrusanov <stanislavhrusanov@gmail.com>
2024-04-07 12:58:59 +0300

    Add cl-interfaces-12-implementing-interfaces

---
+++ Classes-And-Interfaces/cl-interfaces-12-implementing-interfaces/src/app.ts
@@ -0,0 +1,26 @@
+interface Greetable {
+  name: string;
+
+  greet(phrase: string): void;
+}
+
+class Person implements Greetable {
+  name: string;
+  age = 36;
+
+  constructor(n: string) {
+    this.name = n;
+  }
+
+  greet(phrase: string) {
+    console.log(`${phrase} ${this.name}.`);
+  }
+}
+
+let user1: Greetable;
+
+user1 = new Person('Stan');
+
+user1.greet('Hi, I am');
+
+console.log(user1);
