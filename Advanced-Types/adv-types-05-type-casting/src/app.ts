ea4aac68283d622e70a21fb588abc26bdadadf6f
StanislavHrusanov <stanislavhrusanov@gmail.com>
2024-04-08 17:02:44 +0300

    Add adv-types-05-type-casting

---
+++ Advanced-Types/adv-types-05-type-casting/src/app.ts
@@ -0,0 +1,4 @@
+// const inputElement = <HTMLInputElement>document.getElementById('user-input')!;
+const inputElement = document.getElementById('user-input')! as HTMLInputElement;
+
+inputElement.value = 'user1';
