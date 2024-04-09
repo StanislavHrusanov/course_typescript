14b85ef5db811f6e0c07b6b21760cbaf660c7c63
StanislavHrusanov <stanislavhrusanov@gmail.com>
2024-04-09 13:45:27 +0300

    Add generics-06-finished

---
+++ Generics/generics-06-finished/src/app.ts
@@ -0,0 +1,22 @@
+interface CourseGoal {
+    title: string;
+    description: string;
+    completeUntil: Date;
+}
+
+function createCourseGoal(
+    title: string,
+    description: string,
+    completeUntil: Date
+): CourseGoal {
+    let courseGoal: Partial<CourseGoal> = {};
+    courseGoal.title = title;
+    courseGoal.description = description;
+    courseGoal.completeUntil = completeUntil;
+
+    return courseGoal as CourseGoal;
+}
+
+const names: Readonly<string[]> = ['Stan', 'John'];
+
+// names.push('Ivan');
