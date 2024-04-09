09ff9a17c0f696e0831c40b51a97e4bc1f643b64
StanislavHrusanov <stanislavhrusanov@gmail.com>
2024-04-09 13:16:06 +0300

    Add generics-05-generic-classes

---
+++ Generics/generics-05-generic-classes/src/app.ts
@@ -0,0 +1,26 @@
+class DataStorage<T extends string | number | boolean>{
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    if (this.data.indexOf(item) === -1) {
+      return;
+    }
+
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItems() {
+    return [...this.data];
+  }
+}
+
+const textStorage = new DataStorage<string>();
+
+textStorage.addItem('Stan');
+textStorage.addItem('John');
+textStorage.removeItem('Stan');
+console.log(textStorage.getItems());
