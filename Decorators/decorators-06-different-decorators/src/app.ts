6b932f7e6bd9cb80a884c26cb57850367e6161ef
StanislavHrusanov <stanislavhrusanov@gmail.com>
2024-04-11 12:44:28 +0300

    Add decorators-06-different-decorators

---
+++ Decorators/decorators-06-different-decorators/src/app.ts
@@ -0,0 +1,50 @@
+function Log(target: any, propName: string | Symbol) {
+  console.log('Property decorator!');
+  console.log(target, propName);
+}
+
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+  console.log('Accessor decorator!');
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+function Log3(target: any, name: string, descriptor: PropertyDescriptor) {
+  console.log('Method decorator!');
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+function Log4(target: any, name: string | Symbol, position: number) {
+  console.log('Parameter decorator!');
+  console.log(target);
+  console.log(name);
+  console.log(position);
+}
+
+class Product {
+  @Log
+  title: string;
+  private _price: number;
+
+  @Log2
+  set price(value: number) {
+    if (value > 0) {
+      this._price = value;
+    } else {
+      throw new Error('Invalid price - should be a positive number')
+    }
+  }
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+
+  @Log3
+  getPriceWithTax(@Log4 tax: number) {
+    return this._price * (1 + tax);
+  }
+}
