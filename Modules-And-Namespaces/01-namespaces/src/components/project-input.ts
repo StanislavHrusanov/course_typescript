420e88ebf4e2fca626147eafcb3b3a8b9f90930e
StanislavHrusanov <stanislavhrusanov@gmail.com>
2024-04-18 18:07:22 +0300

    Add 01-namespaces

---
+++ Modules-And-Namespaces/01-namespaces/src/components/project-input.ts
@@ -0,0 +1,79 @@
+/// <reference path='base-component.ts' />
+/// <reference path='../decorators/autobind.ts' />
+/// <reference path='../util/validation.ts' />
+/// <reference path='../state/project-state.ts' />
+
+namespace App {
+    export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
+        titleInputElement: HTMLInputElement;
+        descriptionInputElement: HTMLInputElement;
+        peopleInputElement: HTMLInputElement;
+
+        constructor() {
+            super('project-input', 'app', true, 'user-input');
+
+            this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
+            this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
+            this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
+
+            this.configure();
+        }
+
+        configure() {
+            this.element.addEventListener('submit', this.submitHandler);
+        }
+
+        renderContent() { }
+
+        private gatherUserInput(): [string, string, number] | void {
+            const enteredTitle = this.titleInputElement.value;
+            const enterdDescription = this.descriptionInputElement.value;
+            const enteredPeople = this.peopleInputElement.value;
+
+            const validatableTitle: Validatable = {
+                value: enteredTitle,
+                required: true
+            };
+
+            const validatableDescription: Validatable = {
+                value: enterdDescription,
+                required: true,
+                minLength: 5
+            };
+
+            const validatablePeople: Validatable = {
+                value: Number(enteredPeople),
+                required: true,
+                min: 1,
+                max: 5
+            };
+
+            if (!validate(validatableTitle) ||
+                !validate(validatableDescription) ||
+                !validate(validatablePeople)) {
+                alert('Invalid input, try again!');
+                return;
+            } else {
+                return [enteredTitle, enterdDescription, Number(enteredPeople)];
+            }
+        }
+
+        private clearInputs() {
+            this.titleInputElement.value = '';
+            this.descriptionInputElement.value = '';
+            this.peopleInputElement.value = '';
+        }
+
+        @Autobind
+        private submitHandler(event: Event) {
+            event.preventDefault();
+            const gatheredInput = this.gatherUserInput();
+
+            if (Array.isArray(gatheredInput)) {
+                const [title, desc, people] = gatheredInput;
+                projectState.addProject(title, desc, people);
+                this.clearInputs()
+            }
+        }
+    }
+}
---
+++ Modules-And-Namespaces/01-namespaces/src/models/drag-drop.ts
@@ -0,0 +1,12 @@
+namespace App {
+    export interface Dragable {
+        dragStartHandler(event: DragEvent): void;
+        dragEndHandler(event: DragEvent): void;
+    }
+
+    export interface DragTargrt {
+        dragOverHandler(event: DragEvent): void;
+        dropHandler(event: DragEvent): void;
+        dragLeaveHandler(event: DragEvent): void;
+    }
+}
